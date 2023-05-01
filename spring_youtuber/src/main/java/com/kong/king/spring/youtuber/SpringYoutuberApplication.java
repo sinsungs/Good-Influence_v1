36de1f72d9d4d0dd33d86c77f0616c96ac827fa3
sinsungs <39232875+sinsungs@users.noreply.github.com>
2023-05-01 20:39:23 +0900

    spring start

---
+++ spring_youtuber/src/main/java/com/kong/king/spring/youtuber/SpringYoutuberApplication.java
@@ -0,0 +1,13 @@
+package com.kong.king.spring.youtuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringYoutuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringYoutuberApplication.class, args);
+	}
+
+}
---
+++ spring_youtuber/src/test/java/com/kong/king/spring/youtuber/SpringYoutuberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kong.king.spring.youtuber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringYoutuberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
