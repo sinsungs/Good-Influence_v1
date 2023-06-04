836f1c83e85cac30e087805417cdb36b4537ae60
ss <rkdtlstjd123@naver.com>
2023-06-04 23:58:12 +0900

    react youtuber page update

--- react-youtuber/src/App.js
+++ react-youtuber/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Youtuber from './pages/Youtuber';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route component={NotFound} /> */}
         </Routes>
         <Footer />
+        <Youtuber/>
       </div>
     </Router>
   );
--- react-youtuber/src/pages/Youtuber.js
+++ react-youtuber/src/pages/Youtuber.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import '../App.css';
 
 function Youtuber() {
-  return (
-    <div>
-      {/* 컴포넌트 내용 */}
-    </div>
-  );
-}
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     username: '',
+  //     email: '',
+  //     password: ''
+  //   };
+  // }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  // handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   // 등록 로직 작성
+  // }
+    return (
+      <div className="container">
+        <h1>유튜버 등록</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="username">사용자 이름:</label>
+          <input type="text" id="username" name="username" value={this.state.username} onChange={this.handleChange} required />
+          
+          <label htmlFor="email">이메일:</label>
+          <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+          
+          <label htmlFor="password">비밀번호:</label>
+          <input type="password" id="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+          
+          <input type="submit" value="등록" />
+        </form>
+      </div>
+    );
+  }
 
 export default Youtuber;
