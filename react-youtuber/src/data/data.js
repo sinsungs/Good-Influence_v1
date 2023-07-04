2e29d94e66589bcc87c8cfb1d271dbc9523fcf1b
ss <rkdtlstjd123@naver.com>
2023-07-04 22:10:36 +0900

    [Feat] 유튜버 등록 모달창 추가

--- react-youtuber/src/data/data.js
+++ react-youtuber/src/data/data.js
@@ -3,43 +3,71 @@ let data = [
         id: 1,
         title: '박스 1',
         description: '프론트엔드에 디자인 시스템 적용하기',
-        author: 'seo_namu'
+        author: '신성',
+        likes: 10
       },
       {
         id: 2,
         title: '2번째 박스',
         description: '프론트엔드에 디자인 시스템 적용하기',
-        author: 'seo_namu'
+        author: '종찬',
+        likes: 20
       },
       {
         id: 3,
         title: '세 번째 박스',
         description: '프론트엔드에 디자인 시스템 적용하기',
-        author: 'seo_namu'
+        author: '황주',
+        likes: 30
       },
       {
         id: 4,
         title: '4번째 박스',
         description: '프론트엔드에 디자인 시스템 적용하기',
-        author: 'seo_namu'
+        author: '유현',
+        likes: 40
       },
       {
         id: 5,
         title: '5번째 박스',
         description: '프론트엔드에 디자인 시스템 적용하기',
-        author: 'seo_namu'
+        author: '태권',
+        likes: 50
       },
       {
         id: 6,
         title: '6번째 박스',
         description: '프론트엔드에 디자인 시스템 적용하기',
-        author: 'seo_namu'
+        author: '상희',
+        likes: 60
       },
       {
         id: 7,
         title: '7번째 박스',
         description: '프론트엔드에 디자인 시스템 적용하기',
-        author: 'seo_namu'
+        author: '준규',
+        likes: 70
+      },
+      {
+        id: 8,
+        title: '8번째 박스',
+        description: '프론트엔드에 디자인 시스템 적용하기',
+        author: '민지',
+        likes: 80
+      },
+      {
+        id: 9,
+        title: '9번째 박스',
+        description: '프론트엔드에 디자인 시스템 적용하기',
+        author: '보경',
+        likes: 90
+      },
+      {
+        id: 10,
+        title: '10번째 박스',
+        description: '프론트엔드에 디자인 시스템 적용하기',
+        author: '효주',
+        likes: 99
       }
   ] 
   
--- react-youtuber/src/pages/Youtuber.js
+++ react-youtuber/src/pages/Youtuber.js
@@ -6,11 +6,41 @@ import data from '../data/data.js';
 
 import React, {useState, useEffect} from 'react';
 
-function YoutuberList() {
+function Youtuber() {
+
+
+  const [showModal, setShowModal] = useState(false);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Do something with the entered data (e.g., send it to an API)
+
+    // Reset the form inputs
+    setTitle('');
+    setDescription('');
+    setAuthor('');
+  };
 
   return (
     <div className="App">
       <div className="container">
+
+        <button className="login-button" onClick={handleOpenModal}>
+          유튜버 등록하기
+        </button>
+
         {data.map(youtuber => (
           <div className="y-box" key={youtuber.id}>
             <div className="y-box-header">{youtuber.title}</div>
@@ -20,14 +50,45 @@ function YoutuberList() {
                 <span className='y-box-name'>by {youtuber.author}</span>
               </a>
               <span className='y-box-like'>
-                  ♥ 33
+                  ♥ {youtuber.likes}
               </span>
             </div>
           </div>
         ))}
+
+
+{showModal && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={handleCloseModal}>
+                &times;
+              </span>
+              <h2>유튜버 등록하기</h2>
+              <form onSubmit={handleSubmit}>
+                <label>
+                  Title:
+                  <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+                </label>
+                <label>
+                  Description:
+                  <textarea
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </label>
+                <label>
+                  Author:
+                  <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
+                </label>
+                <button type="submit">등록</button>
+              </form>
+            </div>
+          </div>
+        )}
+
       </div>
     </div>
   );
 }
 
-export default YoutuberList;
+export default Youtuber;
