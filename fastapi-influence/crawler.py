6e5e9b794fdda1b15f70f49587426ab6078e3497
ss <rkdtlstjd123@naver.com>
2023-10-01 20:00:35 +0900

    [Feat] 인스타그램 자동 로그인 로직

--- fastapi-influence/crawler.py
+++ fastapi-influence/crawler.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-# import time
+from selenium.webdriver.common.keys import Keys
+import time
 
 # options = Options()
 # options.add_experimental_option('detach', True) # 브라우저 바로 닫힘 방지
@@ -24,15 +25,37 @@ def crawl_naver():
     
     driver = webdriver.Chrome(service=service, options=options)
     
-    driver.get('https://naver.com')
+    # driver.get('https://naver.com')
+    driver.get('https://www.instagram.com/accounts/login/')
     
-    title = "API TEST"
     
     # 크롤링 로직을 여기에 추가
     
-    # 예를 들어, 타이틀 가져오기
-    # title = driver.find_element(By.TAG_NAME, 'title').text
+    driver.implicitly_wait(3)
+    id = driver.find_element(By.NAME, 'username')
+    id.send_keys('daegu_on')
+    time.sleep(2)
     
-    # driver.quit()
+    password = driver.find_element(By.NAME, 'password')
+    password.send_keys('tlstjd0045')
+    time.sleep(2)
+    
+    login_button = driver.find_element(By.CSS_SELECTOR, 'button[type=submit]').click()
+    time.sleep(5)
+    
+    # 로그인 정보 저장 여부 ("나중에 하기 버튼 클릭")
+    btn_later1 = driver.find_element(By.CLASS_NAME,'_ac8f')
+    btn_later1.click()
+    time.sleep(5)
+    
+    # 알림 설정 ("나중에 하기 버튼 클릭")
+    btn_later2 = driver.find_element(By.CLASS_NAME,'_a9-z')
+    btn_later2.click()
+    time.sleep(5)
+
+
+
+    
+    title = "API TEST"
     
     return title
