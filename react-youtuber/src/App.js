66ad63a486b8bf83bc80b9b9ce5e702dabf1b05c
ss <rkdtlstjd123@naver.com>
2023-10-09 16:23:09 +0900

    [fix] vercel.json 수정

--- react-youtuber/src/App.js
+++ react-youtuber/src/App.js
@@ -57,6 +57,9 @@ function App() {
 
         <Routes>
 
+          <Route path="/" element={<Meet/>}/>
+          <Route path="/*" element={<Meet/>}/>
+          <Route path="/" element={<Meet/>}/>
           <Route path="/meet" element={<Meet/>}/>
           <Route path="/Influencer" element={<Influencer/>}/>
           <Route path="/post" element={<RecommendPost/>}/>
